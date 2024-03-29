4c795ba01c50ca6a87105d2513720aa32893f5d9
MetaGG2 <spinosaurus882@gmail.com>
2024-03-29 18:47:41 +0530

    can be properly toggled

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,95 @@ using namespace geode::prelude;
 
 
 #include <Geode/binding/GJGameLevel.hpp>
+#include <Geode/binding/CCMenuItemToggler.hpp>
+#include <Geode/binding/GameLevelOptionsLayer.hpp>
+#include <Geode/binding/GJBaseGameLayer.hpp>
+
+const int DISABLE_SHAKE_BUTTON_INDEX = 2;
+bool enabled = Mod::get()->getSettingValue<bool>("enabled");
 
 #include <Geode/modify/GJBaseGameLayer.hpp>
 class $modify(GJBaseGameLayer) {
 	TodoReturn shakeCamera(float p0, float p1, float p2) {
-		return;
+		if (enabled) GJBaseGameLayer::shakeCamera(p0, p1, p2);
 	}
 
 	TodoReturn applyShake(cocos2d::CCPoint& p0) {
-		return;
+		if (enabled) GJBaseGameLayer::applyShake(p0);
 	}
 };
 
-// #include <Geode/modify/LevelInfoLayer.hpp>
-// class $modify(LevelInfoLayer) {
-// 	static LevelInfoLayer* create(GJGameLevel* level, bool p1) {
-// 		LevelInfoLayer* layer = LevelInfoLayer::create(level, p1)	;
-// 		level->m_disableShake = false;
-// 		return layer;
-// 	}
+#include <Geode/modify/GameLevelOptionsLayer.hpp>
+class $modify(NSGameLevelOptionsLayer, GameLevelOptionsLayer) {
+	bool init(GJGameLevel* level) {
+		if (!GameLevelOptionsLayer::init(level))
+			return false;
+		if (!enabled)
+			return true;
+		auto disableShakeButton = reinterpret_cast<CCMenuItemToggler*>(this->m_buttonMenu->getChildren()->objectAtIndex(1));
+		disableShakeButton->m_notClickable = true;
+		auto color = typeinfo_cast<CCRGBAProtocol*>(disableShakeButton->m_onButton);
+		color->setColor({
+			127,
+			127,
+			127
+		});
+		// auto onSprite = CCSprite::createWithSpriteFrameName("GJ_checkOff_001.png");
+		// onSprite->setScale(0.8f);
+		// auto offSprite = CCSprite::createWithSpriteFrameName("GJ_checkOn_001.png");
+		// offSprite->setScale(0.8f);
+		// auto disableShakeButton = CCMenuItemToggler::create(
+		// 	offSprite,
+		// 	onSprite,
+		// 	this,
+		// 	menu_selector(NSGameLevelOptionsLayer::onToggle)
+		// );
+		// this->m_buttonMenu->getChildren()->removeObjectAtIndex(1);
+		// this->m_buttonMenu->addChild(disableShakeButton);
+		// disableShakeButton->setAnchorPoint({0.5f, 0.5f});
+		// disableShakeButton->setPosition({32, 80});
+		// disableShakeButton->setZOrder(5);
+		// disableShakeButton->m_toggled = true;
+		// disableShakeButton->m_notClickable = true;
+		return true;
+	}
 
-// 	// bool init(GJGameLevel* level, bool p1) {
-// 	// 	if (!LevelInfoLayer::init(level, p1))
-// 	// 		return false;
-// 	//
-// 	// 	this->m_level->m_disableShake = true;
-// 	// 	return true;
-// 	// }
-// };
+	// void onToggle(cocos2d::CCObject* _) {
+	// 	enabled = !enabled;
+	// }
+};
+
+
+
+/*
+the method that doesnt work cuz of robs wonderfulness
+m_disableShake doesn't do anything
+
+#include <Geode/modify/LevelInfoLayer.hpp>
+class $modify(LevelInfoLayer) {
+	static LevelInfoLayer* create(GJGameLevel* level, bool p1) {
+		GameLevelOptionsLayer* options = GameLevelOptionsLayer::create(level);
+
+		LevelInfoLayer* layer = LevelInfoLayer::create(level, p1);
+		return layer;
+	}
+
+	void downloadLevel() {
+		LevelInfoLayer::downloadLevel();
+		this->m_level->m_disableShake = true;
+	}
+
+	void onPlay(cocos2d::CCObject* sender) {
+		this->m_level->m_disableShake = true;
+		LevelInfoLayer::onPlay(sender);
+	}
+
+	bool init(GJGameLevel* level, bool p1) {
+		if (!LevelInfoLayer::init(level, p1))
+			return false;
+
+		this->m_level->m_disableShake = true;
+		return true;
+	}
+};
+*/
